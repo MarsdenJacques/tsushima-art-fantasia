dbe0b0f42320f5a937eabdad1ccbf72083f7dd2d
MarsdenJacques <marsdensgardens@gmail.com>
2021-08-27 22:39:54 +0900

    google lied to me, fixing some camelcasing

--- components/gallery.js
+++ components/gallery.js
@@ -1,7 +1,7 @@
 export default function Gallery(){
     return(
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/q5N5Z6FPB20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/q5N5Z6FPB20" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>    
         </div>
     )
 }
